dd287c55e6b46c97424c55bd914aca278a150041
Shamimayesmin <shamimayesmin144@gmail.com>
2023-04-09 15:18:38 +0600

    six

--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -27,10 +27,7 @@ const TopRated = () => {
       <h1>This is featured page</h1>
       <h1>This is featured page</h1>
       <h1>This is featured page</h1>
-      <h1>This is featured page</h1>
-      <h1>This is featured page</h1>
-      <h1>This is featured page</h1>
-      <h1>This is featured page</h1>
+      
     </div>
   );
 };
