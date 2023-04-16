ed0746bb5ad87058fe1674dd44d71362116e32cf
Shamimayesmin <shamimayesmin144@gmail.com>
2023-04-16 23:40:13 +0600

    all done

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,15 +18,6 @@ const About = () => {
       <h1>This is about page</h1>
       <h1>This is about page</h1>
       <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
       
     </div>
   );
