4692317893350d73886095c9f6085dccd86f53f5
Shamimayesmin <shamimayesmin144@gmail.com>
2023-04-23 23:36:05 +0600

    top rated

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,10 +9,6 @@ const About = () => {
       <h1>This is about page</h1>
       <h1>This is about page</h1>
       <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
       
       
     </div>
